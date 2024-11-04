ab0d75c319387e559f0b894ef6677dbc897b6e34
Nathan Osman <nathan@quickmediasolutions.com>
2024-11-03 21:45:59 -0800

    Add tests for successful requests.

--- test/test.tsx
+++ test/test.tsx
@@ -7,6 +7,7 @@ import { renderHook } from '@testing-library/react'
 import { ApiProvider, useApi } from '../src'
 
 const testPath = '/'
+const testValue = { "test": 123 }
 
 describe('testing ApiProvider', () => {
 
@@ -31,6 +32,18 @@ describe('testing ApiProvider', () => {
   it('throws an error for HTTP errors', async () => {
     fetch.mockOnce('', { status: 400 })
     await expect(result.current.get(testPath)).rejects.toThrow()
+  })
+
+  it('correctly parses JSON in a response', async () => {
+    fetch.mockOnce(JSON.stringify(testValue))
+    await expect(result.current.get(testPath)).resolves.toStrictEqual(testValue)
+  })
+
+  it('correctly sends JSON in request body', async () => {
+    fetch.mockOnce('{}')
+    await expect(result.current.post(testPath, testValue)).resolves.toBeDefined()
+    expect(fetch.mock.calls.length).toEqual(1)
+    expect(fetch.mock.calls[0][1]?.body).toStrictEqual(testValue)
   })
 
 })
