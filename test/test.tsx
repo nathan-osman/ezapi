781551e435e93b94aeaeb83ef3260c5c9055e36c
Nathan Osman <nathan@quickmediasolutions.com>
2024-11-10 23:22:24 -0800

    Reorganize tests.

--- test/test.tsx
+++ test/test.tsx
@@ -32,22 +32,21 @@ const TestTypeSchema = z.object({
   test: z.string(),
 })
 
-describe('testing ApiProvider', () => {
+const { result } = renderHook(() => useApi(), {
+  wrapper: (props) => (
+    <ApiProvider
+      base={testBase}
+      headers={testHeaders}
+    >
+      {props.children}
+    </ApiProvider>
+  ),
+})
+
+describe('testing ApiProvider (fetch)', () => {
 
   beforeEach(() => {
     fetch.resetMocks()
-    XMLHttpRequestMock.resetMocks()
-  })
-
-  const { result } = renderHook(() => useApi(), {
-    wrapper: (props) => (
-      <ApiProvider
-        base={testBase}
-        headers={testHeaders}
-      >
-        {props.children}
-      </ApiProvider>
-    ),
   })
 
   it('throws an error for aborted requests', async () => {
@@ -65,19 +64,6 @@ describe('testing ApiProvider', () => {
     await expect(result.current.get(TestTypeSchema, testPath)).rejects.toThrow()
   })
 
-  it('correctly parses JSON in a response', async () => {
-    fetch.mockOnce(JSON.stringify(testValueGood))
-    await expect(result.current.get(TestTypeSchema, testPath)).resolves.toStrictEqual(testValueGood)
-  })
-
-  it('correctly handles base and headers', async () => {
-    fetch.mockOnce(JSON.stringify(testValueGood))
-    await expect(result.current.get(TestTypeSchema, testPath)).resolves.toBeDefined()
-    expect(fetch.mock.calls.length).toEqual(1)
-    expect(fetch.mock.calls[0][0]).toEqual(`${testBase}${testPath}`)
-    expect(fetch.mock.calls[0][1]?.headers).toStrictEqual(testHeaders)
-  })
-
   it('throws an error for wrong type', async () => {
     fetch.mockOnce(JSON.stringify(testValueBad1))
     await expect(result.current.get(TestTypeSchema, testPath)).rejects.toThrow()
@@ -88,11 +74,37 @@ describe('testing ApiProvider', () => {
     await expect(result.current.get(TestTypeSchema, testPath)).rejects.toThrow()
   })
 
+  it('correctly handles base and headers', async () => {
+    fetch.mockOnce(JSON.stringify(testValueGood))
+    await expect(result.current.get(TestTypeSchema, testPath)).resolves.toBeDefined()
+    expect(fetch.mock.calls.length).toEqual(1)
+    expect(fetch.mock.calls[0][0]).toEqual(`${testBase}${testPath}`)
+    expect(fetch.mock.calls[0][1]?.headers).toStrictEqual(testHeaders)
+  })
+
+  it('correctly parses JSON in a response', async () => {
+    fetch.mockOnce(JSON.stringify(testValueGood))
+    await expect(result.current.get(TestTypeSchema, testPath)).resolves.toStrictEqual(testValueGood)
+  })
+
   it('correctly sends JSON in request body', async () => {
     fetch.mockOnce(JSON.stringify(testValueGood))
     await expect(result.current.post(TestTypeSchema, testPath, testValueGood)).resolves.toBeDefined()
     expect(fetch.mock.calls.length).toEqual(1)
     expect(JSON.parse(fetch.mock.calls[0][1]?.body as string)).toStrictEqual(testValueGood)
+  })
+})
+
+describe('testing ApiProvider (XMLHttpRequest)', () => {
+
+  beforeEach(() => {
+    XMLHttpRequestMock.mockData(400, JSON.stringify(testValueGood))
+
+  })
+
+  it('throws an error for HTTP errors', async () => {
+    fetch.mockOnce('', { status: 400 })
+    await expect(result.current.get(TestTypeSchema, testPath)).rejects.toThrow()
   })
 
   it('correctly sends FormData using XmlHttpRequest', async () => {
